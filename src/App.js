65eb85fe1431c69652ff1bd3b490e831f359abdc
nehaexcellence <neha@excellencetechnologies>
2020-06-15 15:13:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const additem = () => {
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text"  value={value} onChange={e => setValue(e.target.value)} />
+      <button onClick = {() => additem()}>Add </button>
     </div>
   );
 }
+function App() {
+  const [todos, setTodos] = useState([ ]);
+
+  const addTodo = texts => {
+    const newTodos = [...todos, { texts }];
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+             todo={todo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+function Todo({ todo, index }) {
+  return (
+    <div className="todo" >
+      {todo.texts}
+    </div>
+  );
+}
+
 
 export default App;
